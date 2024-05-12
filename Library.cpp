1bed8867bb650e6f9fd7922559d1025fa58f061c
Bushrah Zulfiqar <167017580+Bushrahhh@users.noreply.github.com>
2024-05-12 05:21:33 +0500

    Library.cpp

--- Library.cpp
+++ Library.cpp
@@ -1,80 +1,111 @@
-#include "Library.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
-void Library::LoadUser() { // Load User.txt
-    User olduser;
-    string str;
-    ifstream fin("User.txt"); // Open and read User.txt
-    if(!fin) { // If can't open
-        cerr << "User.txt can't open" << endl;
-        abort(); // Exit
-    }
-    while (getline(fin, str)) {
-        size_t i = str.find(" "); // Find fisrt spacebar
-        olduser.SetUsername(str.substr(0, i)); // Divide str by spacebar and get username
-        str = str.substr(i+1);
+using namespace std;
 
-        i = str.find(" "); // Find second spacebar
-        olduser.SetPassword(str.substr(0, i)); // Divide str by spacebar and get password
-        olduser.SetStatus(stoi(str.substr(i+1))); // Divide str by spacebar and get status
+class User {
+private:
+    string username;
+    string password;
+    int status;
 
-        UserArray.push_back(olduser); // Add to lib.UserArray
-    }
-    fin.close(); // Close User.txt
+public:
+    // Constructor
+    User(const string& username, const string& password, int status) : username(username), password(password), status(status) {}
+
+    // Getters
+    string getUsername() const { return username; }
+    string getPassword() const { return password; }
+    int getStatus() const { return status; }
+
+    // Setters
+    void setUsername(const string& username) { this->username = username; }
+    void setPassword(const string& password) { this->password = password; }
+    void setStatus(int status) { this->status = status; }
+};
+
+class Book {
+private:
+    string name;
+    string isbn;
+    string author;
+    int number;
+    int category;
+    int price;
+    double discount;
+    int disc_num;
+    string information;
+
+public:
+    // Constructor
+    Book(const string& name, const string& isbn, const string& author, int number, int category, int price, double discount, int disc_num, const string& information) : 
+        name(name), isbn(isbn), author(author), number(number), category(category), price(price), discount(discount), disc_num(disc_num), information(information) {}
+
+    // Getters
+    string getName() const { return name; }
+    string getIsbn() const { return isbn; }
+    string getAuthor() const { return author; }
+    int getNumber() const { return number; }
+    int getCategory() const { return category; }
+    int getPrice() const { return price; }
+    double getDiscount() const { return discount; }
+    int getDiscNum() const { return disc_num; }
+    string getInformation() const { return information; }
+
+    // Setters
+    void setName(const string& name) { this->name = name; }
+    void setIsbn(const string& isbn) { this->isbn = isbn; }
+    void setAuthor(const string& author) { this->author = author; }
+    void setNumber(int number) { this->number = number; }
+    void setCategory(int category) { this->category = category; }
+    void setPrice(int price) { this->price = price; }
+    void setDiscount(double discount) { this->discount = discount; }
+    void setDiscNum(int disc_num) { this->disc_num = disc_num; }
+    void setInformation(const string& information) { this->information = information; }
+};
+
+class Library {
+private:
+    vector<User> userArray;
+    vector<Book> bookArray;
+    int income;
+    int outcome;
+
+public:
+    // Constructor
+    Library() : income(0), outcome(0) {}
+
+    // Methods
+    void addUser(const User& user) { userArray.push_back(user); }
+    void addBook(const Book& book) { bookArray.push_back(book); }
+    void loadUser(); // Implementation will be added
+    void loadBook(); // Implementation will be added
+    void loadCash(); // Implementation will be added
+};
+
+void Library::loadUser() {
+    // Simulated loading of user data from storage
+    // This method will populate the userArray with data
 }
 
-void Library::LoadBook() { // Load Book.txt
-    Book oldbook;
-    string str;
-    ifstream fin("Book.txt"); // Open and read Book.txt
-    if(!fin) { // If can't open
-        cerr << "Book.txt can't open" << endl;
-        abort(); // Exit
-    }
-    while (getline(fin, str)) {
-        size_t i = str.find(" "); // Find fisrt spacebar
-        oldbook.SetName(str.substr(0, i)); // Divide str by spacebar and get bookname
-        str = str.substr(i+1);
-
-        i = str.find(" "); // Find second spacebar
-        oldbook.SetIsbn(str.substr(0, i)); // Divide str by spacebar and get ISBN number
-        str = str.substr(i+1);
-
-        i = str.find(" ");
-        oldbook.SetAuthor(str.substr(0, i)); // Divide str by spacebar and get author
-        str = str.substr(i+1);
-
-        i = str.find(" ");
-        oldbook.SetNumber(stoi(str.substr(0, i))); // Divide str by spacebar and get copy number
-        str = str.substr(i+1);
-
-        i = str.find(" ");
-        oldbook.SetCategory(stoi(str.substr(0, i))); // Divide str by spacebar and get category number
-        str = str.substr(i+1);
-
-        i = str.find(" ");
-        oldbook.SetPrice(stoi(str.substr(0, i))); // Divide str by spacebar and get price
-        str = str.substr(i+1);
-
-        i = str.find(" ");
-        oldbook.SetDiscount(stod(str.substr(0, i))); // Divide str by spacebar and get discount
-        str = str.substr(i+1);
-
-        i = str.find(" ");
-        oldbook.SetDisc_num(stoi(str.substr(0, i))); // Divide str by spacebar and get disc_num
-        oldbook.SetInformation(str.substr(i+1)); // Divide str by spacebar and get more book information
-
-        BookArray.push_back(oldbook); // Add to lib.BookArray
-    }
-    fin.close(); // Close Book.txt
+void Library::loadBook() {
+    // Simulated loading of book data from storage
+    // This method will populate the bookArray with data
 }
 
-void Library::LoadCash() { // Load Cash.txt
-    ifstream fin("Cash.txt"); // Open and read Book.txt
-    if(!fin) { // If can't open
-        cerr << "Cash.txt can't open" << endl;
-        abort(); // Exit
-    }
-    fin >> income; // Input income
-    fin >> outcome; // Input outcome
-    fin.close(); // Close Cash.txt
+void Library::loadCash() {
+    // Simulated loading of cash data from storage
+    // This method will set the income and outcome
+}
+
+int main() {
+    Library library;
+    library.loadUser();
+    library.loadBook();
+    library.loadCash();
+
+    // Main program logic goes here
+
+    return 0;
 }
