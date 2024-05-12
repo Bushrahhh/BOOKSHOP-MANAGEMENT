9eb4bd186c49392c12f56d4188cdc5a601bdf45b
Zermeene <162930157+Zermeene@users.noreply.github.com>
2024-05-12 06:00:00 +0500

    Add files via upload

--- KK.cpp
+++ KK.cpp
@@ -1,49 +1,73 @@
 #include <iostream>
-#include <conio.h> // For _getch()
+#include <vector>
 
-class Password {
-   public:
+class Book {
+private:
+    std::string name;
+    std::string isbn;
+    std::string author;
+public:
+    Book(const std::string& name, const std::string& isbn, const std::string& author)
+        : name(name), isbn(isbn), author(author) {}
 
-    std::string psw;
-    int length;
-
-    Password() : length(0) {}
-    
-    // Method to input password
-    void InputPassword();
+    std::string GetName() const { return name; }
+    std::string GetIsbn() const { return isbn; }
+    std::string GetAuthor() const { return author; }
 };
 
-void Password::InputPassword() {
-    char temp_c;
-    psw = "";
-    while (true) {
-        temp_c = _getch(); // Get a character without showing it
-        if (temp_c != char(13)) { // If character is ENTER then break
-            switch (temp_c) {
-                case 8: // If character is Backspace
-                    if (length != 0) {
-                        std::cout << "\b \b";
-                        psw = psw.substr(0, length - 1);
-                        length--;
-                    }
-                    break;
-                default:
-                    std::cout << "*"; // Use '*' to replace what you input
-                    psw += temp_c; // Connect each character to string 'psw'
-                    length++;
-                    break;
-            }
-        } else {
-            break;
-        }
+class Staff {
+private:
+    std::vector<Book> books;
+public:
+    void AddBook(const Book& book);
+    void ViewBooks();
+};
+
+void Staff::AddBook(const Book& book) {
+    books.push_back(book);
+    std::cout << "Book added successfully!" << std::endl;
+}
+
+void Staff::ViewBooks() {
+    std::cout << "Listing all books:" << std::endl;
+    for (const auto& book : books) {
+        std::cout << "Name: " << book.GetName() << ", ISBN: " << book.GetIsbn() << ", Author: " << book.GetAuthor() << std::endl;
     }
-    std::cout << std::endl;
 }
 
 int main() {
-    Password password;
-    std::cout << "Enter your password: ";
-    password.InputPassword();
-    std::cout << "Password entered: " << password.psw << std::endl;
+    Staff staff;
+    char choice;
+    std::string name, isbn, author;
+
+    do {
+        std::cout << "Menu:" << std::endl;
+        std::cout << "1. Add a book" << std::endl;
+        std::cout << "2. View all books" << std::endl;
+        std::cout << "0. Exit" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case '1':
+                std::cout << "Enter book name: ";
+                std::cin >> name;
+                std::cout << "Enter book ISBN: ";
+                std::cin >> isbn;
+                std::cout << "Enter book author: ";
+                std::cin >> author;
+                staff.AddBook(Book(name, isbn, author));
+                break;
+            case '2':
+                staff.ViewBooks();
+                break;
+            case '0':
+                std::cout << "Exiting program..." << std::endl;
+                break;
+            default:
+                std::cout << "Invalid choice. Please try again." << std::endl;
+        }
+    } while (choice != '0');
+
     return 0;
 }
